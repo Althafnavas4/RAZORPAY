3d022c020d86a4330431bc5fda830f4807301958
althafnavas4 <nalthaf13@gmail.com>
2025-01-25 09:40:25 +0530

    new file

--- razor/razor/settings.py
+++ razor/razor/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -50,11 +51,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'razor.urls'
+import os
+
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [[os.path.join(BASE_DIR, 'templates')]],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
